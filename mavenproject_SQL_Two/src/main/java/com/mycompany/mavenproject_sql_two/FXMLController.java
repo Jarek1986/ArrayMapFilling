5d220fa08a8818f051fbdbd2ad1963bb999b0d2c
Jarek <Jarek@Jarek-Komputer>
2017-05-17 22:02:35 +0200

    Sql change, thursday evening

--- mavenproject_SQL_Two/src/main/java/com/mycompany/mavenproject_sql_two/FXMLController.java
+++ mavenproject_SQL_Two/src/main/java/com/mycompany/mavenproject_sql_two/FXMLController.java
@@ -2,8 +2,11 @@ package com.mycompany.mavenproject_sql_two;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
@@ -14,10 +17,16 @@ public class FXMLController implements Initializable {
     @FXML
     private Label label;
     
+    //======= Combo box declaration ===========================
+    @FXML
+    private ComboBox<String> ComboBox1TableCreation;
+    ObservableList<String> list1 = FXCollections.observableArrayList("Steel, ASTM A514", "Stainless steel, AISI304", "Aluminum, 6061-T6");
+    
+    
     
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+       ComboBox1TableCreation.setItems(list1);
     }    
 }
