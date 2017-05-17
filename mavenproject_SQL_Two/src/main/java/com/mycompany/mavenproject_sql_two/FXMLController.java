b68a8bba964c524883fb3303d7e3ac01dc0c9c5b
jaroslaw.ziolkowski <jaroslaw.ziolkowski@SFDT03.SCOPEFLUIDICS.local>
2017-05-17 15:45:54 +0200

    Aplikacja, wersja 1

---
+++ mavenproject_SQL_Two/src/main/java/com/mycompany/mavenproject_sql_two/FXMLController.java
@@ -0,0 +1,23 @@
+package com.mycompany.mavenproject_sql_two;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class FXMLController implements Initializable {
+    
+    //=== Anchor panes ======
+ 
+    @FXML
+    private Label label;
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
